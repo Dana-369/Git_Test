db39f21ecaad8fd17ea6c69fc70ae48fe5a776ab
unknown <danasekar500@gmail.com>
2026-07-05 07:54:27 +0530

    anime One piece

--- Test.py
+++ Test.py
@@ -3,5 +3,15 @@ if hungry.lower() == "yes":
     print("Let's get some food!")
     print("What type of food do you want? (pizza/sushi/burger)")
     print("You can choose from pizza, sushi, or burger.")
+    print("Please enter your choice:")
+    choice = input().lower()
+    if choice == "pizza":
+        print("You chose pizza!")
+    elif choice == "sushi":
+        print("You chose sushi!")
+    elif choice == "burger":
+        print("You chose burger!")
+    else:
+        print("Invalid choice.")
 else:
     print("Alright, maybe later.")
