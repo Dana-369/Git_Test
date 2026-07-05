6908382e8a813c5c7a3d35738060b97073b4455d
unknown <danasekar500@gmail.com>
2026-07-05 07:31:18 +0530

    New line add

--- Test.py
+++ Test.py
@@ -1,5 +1,6 @@
 hungry = input("Are you hungry? (yes/no) ")
 if hungry.lower() == "yes":
     print("Let's get some food!")
+    print("What type of food do you want? (pizza/sushi/burger)")
 else:
     print("Alright, maybe later.")
