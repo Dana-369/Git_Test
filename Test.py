7d9cc8b30dc89f449c9b048067dc176d43b9262f
unknown <danasekar500@gmail.com>
2026-07-05 07:58:22 +0530

    main changed the log

--- Test.py
+++ Test.py
@@ -3,5 +3,7 @@ if hungry.lower() == "yes":
     print("Let's get some food!")
     print("What type of food do you want? (pizza/sushi/burger)")
     print("You can choose from pizza, sushi, or burger.")
+
 else:
     print("Alright, maybe later.")
+    print("What would you like to do instead? (read/watch/go for a walk)")
